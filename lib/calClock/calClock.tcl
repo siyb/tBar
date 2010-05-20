4c6026f3acd011d6654f65cbea7b36a2701adc89
Patrick Sturm <siyb@geekosphere.org>
2010-05-20 18:04:27 +0200

    (ADDITION) added option to go back and forth a month / year with calendar

--- lib/calClock/calClock.tcl
+++ lib/calClock/calClock.tcl
@@ -17,7 +17,7 @@ namespace eval geekosphere::tbar::widget::calClock {
 	proc makeCalClock {w args} {
 		variable sys
 		
-		bind Label <Button-1> [namespace code [list calWindow $w %W]]
+		bind Label <Button-1> [namespace code [list actionHandler $w %W]]
 		
 		set sys($w,originalCommand) ${w}_
 		set sys($w,timeDateFormat) "%+"
@@ -27,7 +27,6 @@ namespace eval geekosphere::tbar::widget::calClock {
 		set sys($w,calcolor,hover) "yellow"
 		set sys($w,calcolor,clicked) "red"
 		set sys($w,calcolor,today)  "blue"
-		
 		frame ${w}
 		pack [label ${w}.clock] -side left
 		
@@ -89,38 +88,85 @@ namespace eval geekosphere::tbar::widget::calClock {
 		return [clock format [clock seconds] -format $sys($w,timeDateFormat)]
 	}
 	
-	# create a calendar window
-	# TODO 1.x: add possibility to enter appointments (balloon stuff)
-	proc calWindow {w invokerWindow} {
+
+	proc actionHandler {w invokerWindow} {
 		variable sys
 		
-		if {"$invokerWindow" ne "$w.clock"} { return }
-		
+		if {"$invokerWindow" eq "${w}.clock"} {
+			drawCalendarWindow $w
+		}
+	}
+
+	# create a calendar window
+	# TODO 1.x: add possibility to enter appointments (balloon stuff)
+	proc drawCalendarWindow {w} {
+		variable sys
+		set currentYear [clock format [clock seconds ] -format "%Y" ]
+		set currentMonth [clock format [clock seconds ] -format "%N" ]
 		set calWin ${w}.calendar
 		if {[winfo exists $calWin]} { 
 			destroy $calWin
 			return 
 		}
 		toplevel $calWin -bg $sys($w,background)
-		pack [calwid ${calWin}.cal \
-				-font			{ helvetica 10 bold } \
-				-dayfont			{Arial 10 bold } \
-				-background		$sys($w,background)\
-				-foreground		$sys($w,foreground) \
-				-activebackground	$sys($w,calcolor,hover) \
-				-clickedcolor		$sys($w,calcolor,clicked) \
-				-startsunday		0 \
-				-delay			1000 \
-				-daynames		{Su Mo Tu Wed Th Fr Sa} \
-				-month			[clock format [clock seconds ] -format "%N" ] \
-				-year			[clock format [clock seconds ] -format "%Y" ] \
-				-relief 			groove \
-			]
+		pack [frame ${calWin}.navigate -bg $sys($w,background)]
+		pack [spinbox ${calWin}.navigate.month \
+			-background		$sys($w,background)\
+			-foreground		$sys($w,foreground) \
+			-activebackground	$sys($w,calcolor,hover) \
+			-from			1 \
+			-to				12 \
+			-width			10 \
+			-increment			1 \
+			-command			[list geekosphere::tbar::widget::calClock::updateWrapper $w $calWin]
+		] -side left -fill x -expand 1
+		
+		pack [spinbox ${calWin}.navigate.year \
+			-background		$sys($w,background)\
+			-foreground		$sys($w,foreground) \
+			-activebackground	$sys($w,calcolor,hover) \
+			-from			[expr {$currentYear - 100}] \
+			-to				[expr {$currentYear + 100}] \
+			-increment			1 \
+			-command			[list geekosphere::tbar::widget::calClock::updateWrapper $w $calWin]
+			
+		] -side right -fill x -expand 1
+
+		${calWin}.navigate.year set $currentYear
+		${calWin}.navigate.month set $currentMonth
+		
+		drawCalendar $w $calWin $currentYear $currentMonth
+		
 		# mark today
 		${calWin}.cal configure -mark [eval list [clock format [clock seconds ] -format "%e %N %Y 1 $sys($w,calcolor,today) { Today }" ]]
 		
 		wm geometry $calWin [geekosphere::tbar::util::getNewWindowGeometry_ [winfo rootx $w]  [winfo rooty $w] 200 200 [winfo height $w] [winfo screenheight $w] [winfo screenwidth $w]]
 		wm overrideredirect $calWin 1
+	}
+	
+	proc updateWrapper {w calWin} {
+		drawCalendar $w $calWin [${calWin}.navigate.year get] [${calWin}.navigate.month get]
+	}	
+	
+	proc drawCalendar {w calWin year month} {
+		variable sys
+		if {[winfo exists ${calWin}.cal]} {
+			destroy ${calWin}.cal
+		}
+		pack [calwid ${calWin}.cal \
+			-font				{ helvetica 10 bold } \
+			-dayfont			{Arial 10 bold } \
+			-background		$sys($w,background)\
+			-foreground		$sys($w,foreground) \
+			-activebackground	$sys($w,calcolor,hover) \
+			-clickedcolor		$sys($w,calcolor,clicked) \
+			-startsunday		0 \
+			-delay			1000 \
+			-daynames		{Su Mo Tu Wed Th Fr Sa} \
+			-month			$month \
+			-year				$year \
+			-relief 			groove \
+		]
 	}
 	
 	#

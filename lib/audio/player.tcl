e8f9bcb4e51863f28e83b6f1ad2d666e18b3f7f3
Patrick Sturm <siyb@geekosphere.org>
2010-06-16 00:13:24 +0200

    (ADDITION) added mock events to mpd.tcl (CHANGE) canvas will not fill both any more -> buttons look better

--- lib/audio/player.tcl
+++ lib/audio/player.tcl
@@ -20,7 +20,7 @@ namespace eval geekosphere::tbar::widget::player {
 		set sys($w,foreground) blue
 		
 		frame $w
-		pack [canvas $sys($w,playerCanvas) -highlightthickness 0] -side left -fill both
+		pack [canvas $sys($w,playerCanvas) -highlightthickness 0] -side left
 		pack [label $sys($w,time) -textvariable geekosphere::tbar::widget::player::displayTime] -side right -fill both
 		set geekosphere::tbar::widget::player::displayTime "Not playing"
 		
@@ -46,7 +46,7 @@ namespace eval geekosphere::tbar::widget::player {
 	proc createUiWrapper {w height} {
 		variable sys
 		resetUi $w
-		createUi $w $height [expr {$height / 3}]
+		createUi $w $height [expr {$height / 2}]
 	}
 	
 	proc createUi {w xy offset} {
--- widget/mpd.tcl
+++ widget/mpd.tcl
@@ -7,12 +7,14 @@ namespace eval geekosphere::tbar::wrapper::mpd {
 			-fg $geekosphere::tbar::conf(color,text) \
 			-bg $geekosphere::tbar::conf(color,background) \
 			-font $geekosphere::tbar::conf(font,sysFont) \
-			-height $geekosphere::tbar::conf(geom,height) \
+			-height 10 \
+			-bindplay [list puts play] \
+			-bindpause [list puts pause] \
+			-bindstop [list puts stop] \
 			{*}$settingsList
 		] -side $geekosphere::tbar::conf(widgets,position) -fill both
 		return $path
 	}
-	
 	proc update {path} {
 		$path update
 	}

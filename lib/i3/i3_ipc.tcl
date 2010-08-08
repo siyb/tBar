a54d42bfd62d8cdb844874574eab858190f9377e
Patrick Sturm <siyb@geekosphere.org>
2010-08-08 13:51:42 +0200

    (CHANGE) i3 ipc uses two channels now, one for events and the other one for information

--- lib/i3/i3_ipc.tcl
+++ lib/i3/i3_ipc.tcl
@@ -10,45 +10,66 @@ namespace eval geekosphere::tbar::i3::ipc {
 	# i3 socket file
 	set sys(socketFile) [file join $::env(HOME) .i3 ipc.sock]
 
-	# socket channel
-	set sys(socket) -1
-	
+	# sockets
+	set sys(info_socket) -1
+	set sys(event_socket) -1
+
 	# reply variable, stores reply of i3
-	set sys(reply) ""
+	set sys(info_reply) ""
+	set sys(event_reply) ""
 	
 	proc connect {} {
 		variable sys
-		if {$sys(socket) != -1} { error "Connection already established" }
-		set sys(socket) [unix_sockets::connect $sys(socketFile)]
-		chan configure $sys(socket) -translation binary -blocking 0
-		fileevent $sys(socket) readable geekosphere::tbar::i3::ipc::readSocket
-		return $sys(socket)
+		if {$sys(info_socket) != -1 || $sys(event_socket) != -1} { error "Connection already established" }
+		set sys(info_socket) [unix_sockets::connect $sys(socketFile)]
+		set sys(event_socket) [unix_sockets::connect $sys(socketFile)]
+		
+		chan configure $sys(info_socket) -translation binary -blocking 0
+		chan configure $sys(event_socket) -translation binary -blocking 0
+		
+		fileevent $sys(info_socket) readable [list geekosphere::tbar::i3::ipc::readInfo]
+		fileevent $sys(event_socket) readable [list geekosphere::tbar::i3::ipc::readEvent]
 	}
 
 	proc disconnect {} {
 		variable sys
-		if {$sys(socket) == 1} { error "Connection has not been established" }
-		close $sys(socket)
-		set sys(socket) -1
+		if {$sys(info_socket) != -1 $sys(reply_socket) != -1} { error "Connection has not been established" }
+		close $sys(info_socket)
+		close $sys(reply_socket)
+		set sys(info_socket) -1
+		set sys(reply_socket) -1
 	}
 
-	proc readSocket {} {
+	proc readInfo {} {
+		puts info
 		variable sys
-		set data [read $sys(socket)]
-		set sys(reply) $data
+		set data [read $sys(info_socket)]
+		set sys(info_reply) $data
+		return $data
+	}
+	
+	proc readEvent {} {
+		variable sys
+		set data [read $sys(event_socket)]
+		set sys(event_reply) $data
 		return $data
 	}
 
-	proc sendMessage {type message} {
+	proc sendMessage {socket type message} {
 		variable sys
 		if {$type < 0 || $type > 3} { error "Message type invalid, must be between 0 and 3" }
-		puts $sys(socket) [binary format a*nna* "i3-ipc" [string length $message] $type $message]
-		flush $sys(socket)
+		puts $sys($socket) [binary format a*nna* "i3-ipc" [string length $message] $type $message]
+		flush $sys($socket)
 	}
 	
-	proc addListener {procedure} {
+	proc addInfoListener {procedure} {
 		variable sys
-		trace add variable sys(reply) write $procedure
+		trace add variable sys(info_reply) write $procedure
+	}
+	
+	proc addEventListener {procedure} {
+		variable sys
+		trace add variable sys(event_reply) write $procedure
 	}
 	
 	#
@@ -56,16 +77,21 @@ namespace eval geekosphere::tbar::i3::ipc {
 	#
 	
 	proc getWorkspaces {} {
-		sendMessage 1 ""
+		sendMessage info_socket 1 ""
 	}
 
 	proc getOutputs {} {
-		sendMessage 3 ""
+		sendMessage info_socket 3 ""
 	}
 	
-	proc getReply {} {
+	proc getEvent {} {
 		variable sys
-		return $sys(reply)
+		return $sys(event_reply)
+	}
+	
+	proc getInfo {} {
+		variable sys
+		return $sys(info_reply)
 	}
 	
 	#
@@ -73,40 +99,55 @@ namespace eval geekosphere::tbar::i3::ipc {
 	#
 	
 	proc subscribeToWorkspaceFocus {} {
-		sendMessage 2 {[ "workspace", "focus" ]}
+		sendMessage event_socket 2 {[ "workspace", "focus" ]}
 	}
 	
 	proc subscribeToWorkspaceInit {} {
-		sendMessage 2 {[ "workspace", "init" ]}
+		sendMessage event_socket 2 {[ "workspace", "init" ]}
 	}
 	
 	proc subscribeToWorkspaceEmpty {} {
-		sendMessage 2 {[ "workspace", "empty" ]}
+		sendMessage event_socket 2 {[ "workspace", "empty" ]}
 	}
 	
 	proc subscribeToWorkspaceUrgent {} {
-		sendMessage 2 {[ "workspace", "urgent" ]}
+		sendMessage event_socket 2 {[ "workspace", "urgent" ]}
 	}
 	
 	proc subscribeToOutput {} {
-		sendMessage 2 {["output", "unspecified"]}
+		sendMessage event_socket 2 {["output", "unspecified"]}
 	}
 	
 	namespace export \
-	connect disconnect addListener getReply \
+	connect disconnect addInfoListener addEventListener getEvent getInfo \
 	getWorkspaces getOutputs \
 	subscribeToWorkspaceFocus subscribeToWorkspaceInit subscribeToWorkspaceEmpty subscribeToWorkspaceUrgent \
 	subscribeToOutput
 }
 
-#proc foo {args} {
-#	variable sys
-#	puts "ARGS: $args | Reply: [getReply]"
-#}
+proc foo {args} {
+	puts "ARGS: $args | Reply Info: [getInfo]"
+}
 
-#namespace import geekosphere::tbar::i3::ipc::*
-#connect
-#addListener foo
-#subscribeToWorkspaceFocus
-#getWorkspaces
-#vwait a
+proc bar {args} {
+	puts "ARGS: $args | Reply Event: [getEvent]"
+}
+
+namespace import geekosphere::tbar::i3::ipc::*
+connect
+puts 1
+addInfoListener foo
+puts 2
+addEventListener bar
+puts 3
+subscribeToWorkspaceFocus
+puts 4
+subscribeToWorkspaceInit
+puts 5
+subscribeToWorkspaceEmpty
+puts 6
+subscribeToWorkspaceUrgent
+puts 7
+getWorkspaces
+puts 8
+vwait a

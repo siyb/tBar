16858390d1584cc8fc04b57fcfb3b1d4a0a0569a
Patrick Sturm <siyb@rorschach>
2013-03-02 00:32:45 +0100

    [ADD] memory history (on click), will not record history unless shown (yet)

--- lib/memory/memory.tcl
+++ lib/memory/memory.tcl
@@ -1,8 +1,10 @@
 package provide memory 1.2
-
 if {![info exist geekosphere::tbar::packageloader::available]} {
 	package require statusBar
 	package require util
+
+	# TODO: not declared in widget file yet
+	package require struct
 }
 
 # TODO: infowindow (like cpu windows) with memory history
@@ -70,7 +72,11 @@ namespace eval geekosphere::tbar::widget::memory {
 
 		${w}.memory configure -text $memtext
 		if {$sys($w,useSwap)} { ${w}.swap configure -text $swaptext }
-		
+
+		foreach window [returnNestedChildren $w] {
+			bind $window <Button-1> [namespace code [list displayHistory $w]]
+		}
+
 		# rename widgets so that it will not receive commands
 		uplevel #0 rename $w ${w}_
 
@@ -100,21 +106,15 @@ namespace eval geekosphere::tbar::widget::memory {
 			set swap [usedSwap]
 			set memory [usedMem]
 		}
+
 		${w}.memorystatus update $memory
 		
 		# SWAP
 		if {$sys($w,useSwap)} {
-			# free
-			if {$sys($w,showWhat)} {
-				set swap [string trimright [dict get $sys(memData) "SwapFree"] "kB"]
-			# used
-			} else {
-				set swap [usedSwap]
-			}
 			${w}.swapstatus update $swap
 		}
 		
-		
+		updateHistory $w
 	}
 	
 	proc action {w args} {
@@ -163,7 +163,42 @@ namespace eval geekosphere::tbar::widget::memory {
 		}
 	}
 
-	# get used memory in percent
+	proc displayHistory {w} {
+		variable sys
+		set sys($w,hiscolor) blue
+		set sys($w,memHistory) ${w}.history
+		if {[winfo exists $sys($w,memHistory)]} {
+			destroy $sys($w,memHistory)
+			unset sys($w,memHistory)
+			return
+		}
+		toplevel $sys($w,memHistory)
+		foreach t [list mem swap] {
+			pack [frame $sys($w,memHistory).${t} -height 30] -fill x
+			pack [label $sys($w,memHistory).${t}.l -text $t -fg $sys($w,foreground) -bg $sys($w,background) -font $sys($w,font)] -side left -fill x -expand 1 
+			pack [barChart $sys($w,memHistory).${t}.chart \
+				-fg $sys($w,foreground) \
+				-bg $sys($w,background) \
+				-font $sys($w,font) \
+				-gc $sys($w,hiscolor) \
+				-font $sys($w,font) \
+				-width 100 \
+				-height 20] -side right
+		}
+		positionWindowRelativly $sys($w,memHistory) $w
+	}
+	
+	proc updateHistory {w} {
+		variable sys
+		if {![info exists sys($w,memHistory)]} {
+			return
+		}
+		$sys($w,memHistory).mem.chart pushValue [set percMem [expr {([usedMem] / [totalMem]) * 100.0}]]
+		$sys($w,memHistory).mem.chart update 
+		$sys($w,memHistory).swap.chart pushValue [set percSwap [expr {([usedSwap] / [totalSwap]) * 100.0}]]
+		$sys($w,memHistory).swap.chart update 
+	}
+
 	# used = used - (buffers + cached)
 	proc usedMem {} {
 		variable sys
@@ -171,7 +206,12 @@ namespace eval geekosphere::tbar::widget::memory {
 		set total [string trimright [dict get $sys(memData) "MemTotal"] "kB"]
 		set cached [string trimright [dict get $sys(memData) "Cached"] "kB"]
 		set buffers [string trimright [dict get $sys(memData) "Buffers"] "kB"]
-		return [expr {$total - $free - ($buffers + $cached)}]
+		return [expr {$total - $free - ($buffers + $cached)}].0
+	}
+	
+	proc totalMem {} {
+		variable sys
+		return [string trimright [dict get $sys(memData) "MemTotal"] "kB"].0
 	}
 	
 	# get free memory
@@ -181,15 +221,19 @@ namespace eval geekosphere::tbar::widget::memory {
 		set free [string trimright [dict get $sys(memData) "MemFree"] "kB"]
 		set cached [string trimright [dict get $sys(memData) "Cached"] "kB"]
 		set buffers [string trimright [dict get $sys(memData) "Buffers"] "kB"]
-		return [expr {$free + ($buffers + $cached)}]
+		return [expr {$free + ($buffers + $cached)}].0
 	}
 
-	# get used swap in percent
 	proc usedSwap {} {
 		variable sys
 		set free [string trimright [dict get $sys(memData) "SwapFree"] "kB"]
 		set total [string trimright [dict get $sys(memData) "SwapTotal"] "kB"]
-		return [expr {$total - $free}]
+		return [expr {$total - $free}].0
+	}
+
+	proc totalSwap {} {
+		variable sys
+		return [string trimright [dict get $sys(memData) "SwapTotal"] "kB"].0
 	}
 	
 	# gets all required information from memfile
@@ -203,6 +247,7 @@ namespace eval geekosphere::tbar::widget::memory {
 	
 	proc changeForegroundColor {w color} {
 		variable sys
+		set sys($w,foreground) $color
 		${w}.memory configure -fg $color
 		${w}.memorystatus configure -fg $color
 		if {$sys($w,useSwap)} {
@@ -213,6 +258,7 @@ namespace eval geekosphere::tbar::widget::memory {
 	
 	proc changeBackgroundColor {w color} {
 		variable sys
+		set sys($w,background) $color
 		$sys($w,originalCommand) configure -bg $color
 		${w}.memory configure -bg $color
 		${w}.memorystatus configure -bg $color
@@ -236,6 +282,7 @@ namespace eval geekosphere::tbar::widget::memory {
 	
 	proc changeFont {w font} {
 		variable sys
+		set sys($w,font) $font
 		${w}.memory configure -font $font
 		${w}.memorystatus configure -font $font
 		if {$sys($w,useSwap)} {

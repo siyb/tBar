536191931d6b6d78b631351404a62f22b5eb9fed
Patrick Sturm <siyb@geekosphere.org>
2013-03-25 23:57:51 +0100

    [ADD] started widget console command module

---
+++ lib/tbar/consolecmd/widget.tcl
@@ -0,0 +1,37 @@
+namespace eval geekosphere::tbar::console::command::widget {
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "hasSubCommands" 1
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "subCommands" [list "list" "unload"]
+
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "list" "hasSubCommands" 0
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "list" "info" [list \
+		"geekosphere::tbar::console::command::widget::list" \
+		"" \
+		"Displays information on loaded widgets"]	
+	
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "unload" "hasSubCommand" 0
+	dict set geekosphere::tbar::console::sys(buildinCommand) "widget" "unload" "info" [list \
+		"geekosphere::tbar::console::command::widget::unload" \
+		"Module unloaded" \
+		"Unloads the given module"]
+
+	proc list {} {
+		dict for {name info} $geekosphere::tbar::sys(widget,dict) {
+			dict with info {
+				geekosphere::tbar::console::printMessage "----------------------------------------"
+				geekosphere::tbar::console::printMessage ""
+				geekosphere::tbar::console::printMessage "Id: $name"
+				geekosphere::tbar::console::printMessage "WidgetProc: $widgetName"
+				geekosphere::tbar::console::printMessage "UpdateInterval: $updateInterval"
+				geekosphere::tbar::console::printMessage "Arguments: $arguments"
+				geekosphere::tbar::console::printMessage "Path: $path"
+				geekosphere::tbar::console::printMessage ""
+			}
+		}
+	}
+
+	proc unload {args} {
+		if {[llength $args] > 1} {
+			return;
+		}
+	}
+}

94a9a16b412acc43b5c2a9a30ea2b5f75248bb3d
Patrick Sturm <siyb@geekosphere.org>
2013-03-31 22:36:24 +0200

    [CHG] minor changes

--- track/server.js
+++ track/server.js
@@ -5,6 +5,10 @@ var app = express();
 var port = 9999;
 var host = "127.0.0.1";
 
+app.get("/module/:id", function(req, res) {
+    res.send("OK " + id);
+});
+
 winston.info("Starting server:" + host + ":" + port);
 app.listen(port, host);
 

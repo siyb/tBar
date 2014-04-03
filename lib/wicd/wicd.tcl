7be611baa0c87e985f89de2cb51984725dd7f645
root <root@rorschach>
2014-04-03 23:56:26 +0200

    added signal strength rendering to wicd widget

--- lib/wicd/wicd.tcl
+++ lib/wicd/wicd.tcl
@@ -3,7 +3,6 @@ package provide wicd 1.0
 if {![info exist geekosphere::tbar::packageloader::available]} {
 	package require logger
 	package require wicd_dbus
-	package require barChart
 }
 
 proc wicd {w args} {
@@ -28,13 +27,20 @@ namespace eval geekosphere::tbar::widget::wicd {
 
 		set sys($w,originalCommand) ${w}_
 		set sys($w,networkWindow) ${w}.networkWindow
-		set sys($w,signalStrengthBarChart) ${w}.signalStrength
 		set sys($w,signalStrength) 0
+		set sys($w,signalStrengthId) -1
+		set sys($w,signalStrengthTextId) -1
+		set sys($w,gc,goodSignal) green
+		set sys($w,gc,mediumSignal) yellow
+		set sys($w,gc,badSignal) red
+		set sys($w,background) black
 
 		frame ${w}
 		uplevel #0 rename $w ${w}_
 
-		pack [barChart $sys($w,signalStrengthBarChart) -width 100 -textvariable ::geekosphere::tbar::widget::wicd::sys($w,signalStrength)]
+		set sys($w,canvas) [canvas ${w}.canvas -bg $sys($w,background)]
+		pack $sys($w,canvas)
+
 		foreach window [returnNestedChildren $w] {
 			bind $window <Button-1> [namespace code [list showNetworkWindow $w]]
 		}
@@ -63,6 +69,15 @@ namespace eval geekosphere::tbar::widget::wicd {
 					}
 					"-font" {
 						changeFont $w $value
+					}
+					"-gss" {
+						changeGraphicsColorGoodSignal $w $value
+					}
+					"-mss" {
+						changeGraphicsColorMediumSignal $w $value
+					}
+					"-bss" {
+						changeGraphicsColorBadSignal $w $value
 					}
 					default {
 						error "${opt} not supported"
@@ -105,13 +120,53 @@ namespace eval geekosphere::tbar::widget::wicd {
 		set quality [getQualityFor $currentNetworkId]
 		log "INFO" "Connected to network $currentNetworkId, updating signal strength accordingly ($quality)."
 		set sys($w,signalStrength) $quality
-		$sys($w,signalStrengthBarChart) setValues [lrepeat $quality 100]
-		$sys($w,signalStrengthBarChart) update
+		drawSignalStrength $w
+	}
+
+	proc drawSignalStrength {w} {
+		variable sys
+		if {$sys($w,signalStrengthId) != -1} {
+			$sys($w,canvas) delete $sys($w,signalStrengthId)
+		}
+		if {$sys($w,signalStrengthTextId) != -1} {
+			$sys($w,canvas) delete $sys($w,signalStrengthTextId)
+		}
+		set canvasHeight [winfo height $sys($w,canvas)]
+		set canvasWidth [winfo width $sys($w,canvas)]
+		if {$canvasHeight == 1} {
+			return
+		}
+		set heightOfSignalStrength [expr {($canvasHeight * $sys($w,signalStrength) / 100)}]
+		log "INFO" "canvasHeight: $canvasHeight canvasWidth: $canvasWidth heightOfSignalStrength: $heightOfSignalStrength ($sys($w,signalStrength))"
+
+
+		set sys($w,signalStrengthId) [$sys($w,canvas) create rectangle \
+				0 $canvasHeight \
+				$canvasWidth [expr {$canvasHeight - $heightOfSignalStrength}] \
+			-fill [getColorBySignalStrength $w]]
+
+		set sys($w,signalStrengthTextId) [$sys($w,canvas) create text \
+			[expr {$canvasWidth / 2}] [expr {$canvasHeight / 2}] \
+			-text $sys($w,signalStrength) \
+			-font $sys($w,font) \
+			-fill $sys($w,foreground)]
+	}
+
+	proc getColorBySignalStrength {w} {
+		variable sys
+		if {$sys($w,signalStrength) > 70} {
+			return $sys($w,gc,goodSignal)
+		} elseif {$sys($w,signalStrength > 40} {
+			return $sys($w,gc,mediumSignal)
+		} else {
+			return $sys($w,gc,badSignal)
+		}
 	}
 
 	proc changeBackgroundColor {w color} {
 		variable sys
 		$sys($w,originalCommand) configure -bg $color
+		$sys($w,canvas) configure -bg $color
 		set sys($w,background) $color
 	}
 
@@ -119,7 +174,6 @@ namespace eval geekosphere::tbar::widget::wicd {
 		variable sys
 		$sys($w,originalCommand) configure -bg $color
 		set sys($w,foreground) $color
-
 	}
 
 	proc changeFont {w font} {
@@ -131,12 +185,28 @@ namespace eval geekosphere::tbar::widget::wicd {
 		variable sys
 		set sys($w,width) $width
 		$sys($w,originalCommand) configure -width $width
+		$sys($w,canvas) configure -width $width
 	}
 
 	proc changeHeight {w height} {
 		variable sys
 		set sys($w,height) $height
 		$sys($w,originalCommand) configure -height $height
+		$sys($w,canvas) configure -height $height
 	}
 
+	proc changeGraphicsColorGoodSignal {w color} {
+		variable sys
+		set sys($w,gc,goodSignal) $color
+	}
+
+	proc changeGraphicsColorMediumSignal {w color} {
+		variable sys
+		set sys($w,gc,mediumSignal) $color
+	}
+
+	proc changeGraphicsColorBadSignal {w color} {
+		variable sys
+		set sys($w,gc,badSignal) $color
+	}
 }

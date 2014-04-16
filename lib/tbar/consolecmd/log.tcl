a5af9098fe867751d2f925d3c03a2bef62640d08
Patrick Sturm <siyb@geekosphere.org>
2014-04-17 01:01:51 +0200

    added list logged namespaces feature to console

--- lib/tbar/consolecmd/log.tcl
+++ lib/tbar/consolecmd/log.tcl
@@ -1,6 +1,6 @@
 namespace eval geekosphere::tbar::console::command::log {
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "hasSubCommands" 1
-	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "subCommands" [list "on" "off" "status"]
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "subCommands" [list "on" "off" "status" "ns"]
 
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "on" "hasSubCommands" "0"
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "on" "info" [list \
@@ -20,6 +20,12 @@ namespace eval geekosphere::tbar::console::command::log {
 		"" \
 		"Displays the dispatching status of tBar logs"]
 
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "ns" "hasSubCommands" 0
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "ns" "info" [list \
+		"geekosphere::tbar::console::command::log::listLoggedNamespaces" \
+		"" \
+		"Lists all logged  namespaces"]
+
 	proc logStatus {} {
 		if {$geekosphere::tbar::util::logger::loggerSettings(dispatchCommand) eq "geekosphere::tbar::console::logDispatch"} {
 			geekosphere::tbar::console::printMessage "Log dispatching enabled"
@@ -28,5 +34,25 @@ namespace eval geekosphere::tbar::console::command::log {
 		}
 	}
 
+	proc setLogLevelFor {args} {
 
+	}
+
+	proc listLoggedNamespaces {} {
+		getNestedChildNameSpaces ::geekosphere::tbar stack
+		foreach ns $stack {
+			if {[info exists ${ns}::logger(init)]} {
+				geekosphere::tbar::console::printMessage $ns
+			}
+		}
+	}
+
+	proc getNestedChildNameSpaces {ns result} {
+		upvar 1 $result stack
+		set children [namespace children $ns]
+		lappend stack {*}$children
+		foreach c $children {
+			getNestedChildNameSpaces $c $result
+		}
+	}
 }

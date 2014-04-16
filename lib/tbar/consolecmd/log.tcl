6cf48aff0787923277807550b8da4f5764ea1e98
Patrick Sturm <siyb@geekosphere.org>
2014-04-17 01:28:43 +0200

    log levels can now be adjusted from the console

--- lib/tbar/consolecmd/log.tcl
+++ lib/tbar/consolecmd/log.tcl
@@ -1,6 +1,6 @@
 namespace eval geekosphere::tbar::console::command::log {
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "hasSubCommands" 1
-	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "subCommands" [list "on" "off" "status" "ns"]
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "subCommands" [list "on" "off" "status" "ns" "level"]
 
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "on" "hasSubCommands" "0"
 	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "on" "info" [list \
@@ -26,6 +26,12 @@ namespace eval geekosphere::tbar::console::command::log {
 		"" \
 		"Lists all logged  namespaces"]
 
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "level" "hasSubCommands" 0
+	dict set geekosphere::tbar::console::sys(buildinCommand) "log" "level" "info" [list \
+		"geekosphere::tbar::console::command::log::setLogLevelFor" \
+		"" \
+		"Sets the level for the given namespace"]
+
 	proc logStatus {} {
 		if {$geekosphere::tbar::util::logger::loggerSettings(dispatchCommand) eq "geekosphere::tbar::console::logDispatch"} {
 			geekosphere::tbar::console::printMessage "Log dispatching enabled"
@@ -35,16 +41,40 @@ namespace eval geekosphere::tbar::console::command::log {
 	}
 
 	proc setLogLevelFor {args} {
+		set validLevels [list "FATAL" "ERROR" "WARNING" "INFO" "DEBUG" "TRACE"]
+		if {[llength $args] != 2} {
+			geekosphere::tbar::console::printError "Please specify a namespace an a log level"
+			return
+		}
+		set ns [lindex $args 0]
+		set level [lindex $args 1]
 
+		if  {[lsearch $validLevels $level] == -1} {
+			geekosphere::tbar::console::printError "Invalid log level ($level): $validLevels"
+			return
+		}
+		if  {[lsearch [getLoggedNamespaces ::geekosphere::tbar] $ns] == -1} {
+			geekosphere::tbar::console::printError "Namespace '$ns' is not configured for logging"
+			return
+		}
+		set ${ns}::logger(level) $level
+		geekosphere::tbar::console::printMessage "Level '$level' set for ${ns}::logger(level)"
 	}
 
 	proc listLoggedNamespaces {} {
-		getNestedChildNameSpaces ::geekosphere::tbar stack
-		foreach ns $stack {
-			if {[info exists ${ns}::logger(init)]} {
-				geekosphere::tbar::console::printMessage $ns
+		foreach ns [getLoggedNamespaces ::geekosphere::tbar] {
+			geekosphere::tbar::console::printMessage $ns
+		}
+	}
+
+	proc getLoggedNamespaces {ns} {
+		getNestedChildNameSpaces $ns stack
+		foreach ns_ $stack {
+			if {[info exists ${ns_}::logger(init)]} {
+				lappend ret $ns_
 			}
 		}
+		return $ret
 	}
 
 	proc getNestedChildNameSpaces {ns result} {

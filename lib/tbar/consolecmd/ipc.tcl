51a144d368bd49438b6df9b3b15fa0a29bf783d2
Patrick Sturm <siyb@geekosphere.org>
2014-06-20 15:05:32 +0200

    added ipc run to console commands

--- lib/tbar/consolecmd/ipc.tcl
+++ lib/tbar/consolecmd/ipc.tcl
@@ -1,7 +1,7 @@
 namespace eval geekosphere::tbar::console::command::ipc {
 
 	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "hasSubCommands" 1
-	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "subCommands" [list "show"]
+	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "subCommands" [list "show" "run"]
 
 	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "show" "hasSubCommands" "0"
 	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "show" "info" [list \
@@ -9,11 +9,26 @@ namespace eval geekosphere::tbar::console::command::ipc {
 		"" \
 		"Lists all available IPC actions"]
 
+	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "run" "hasSubCommands" "0"
+	dict set geekosphere::tbar::console::sys(buildinCommand) "ipc" "run" "info" [list \
+		"geekosphere::tbar::console::command::ipc::run" \
+		"" \
+		"Run IPC command"]
+
+
 	proc show {} {
 		set ipcList [geekosphere::tbar::ipc::obtainIPCList]
 		foreach item $ipcList {
 			geekosphere::tbar::console::printMessage $item
 		}	
+	}
+
+	proc run {args} {
+		if {[llength $args] != 1} {
+			geekosphere::tbar::console::printError "Please specify the ipc command to run"
+			return
+		}
+		geekosphere::tbar::ipc::sendIPCCommand {*}[split [lindex $args 0] "#"]
 	}
 
 }
